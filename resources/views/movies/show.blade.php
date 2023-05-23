6ea0d031edb0af1950ef37160f27d8ffbd6b2a02
NelloRossi <rossianiello97@gmail.com>
2023-05-23 17:04:14 +0200

    fixed movie info page

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -2,12 +2,27 @@
 
 @section('content')
     <main>
-        <h1>Movies List</h1>
-        <h2>{{ $movie->title }}</h2>
-        <p>Titolo originale: {{ $movie->original_title }}</p>
-        <p>Nazione: {{ $movie->nationality }}</p>
-        <p>Data di pubblicazione: {{ $movie->date }}</p>
-        <p>Voto: {{ $movie->vote }}</p>
-        <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
+        <section class="d-flex justify-content-between px-5 mt-5">
+            <div class="ps-5">
+                <a href="{{ route('movies.index') }}" class="fw-bold">Torna alla lista dei film</a>
+                <h1 class="mt-5">{{ $movie->title }}</h1>
+                <p>Titolo originale: {{ $movie->original_title }}</p>
+                <p>Nazione: {{ $movie->nationality }}</p>
+                <p>Data di pubblicazione: {{ $movie->date }}</p>
+                <p>Voto: {{ $movie->vote }}</p>
+            </div>
+
+            <div class="pe-5">
+                <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
+            </div>
+
+        </section>
+
     </main>
 @endsection
+
+<style lang="scss" scoped>
+    img {
+        max-height: 500px;
+    }
+</style>
